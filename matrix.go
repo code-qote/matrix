09b9455cdbe78a39a84dd7de1b7a4852ab46fdbb
Glushin Nikita <nikita.glushin@yandex.ru>
2022-10-19 01:01:32 +0300

    First Commit

---
+++ matrix.go
@@ -0,0 +1,207 @@
+package linealgebra
+
+import "errors"
+
+type Size struct {
+	Cols int
+	Rows int
+}
+
+type Matrix[T int | float64] struct {
+	size  *Size
+	array [][]T
+}
+
+type Vector[T int | float64] struct {
+	Matrix[T]
+	size  *Size
+	array [][]T
+}
+
+func NewMatrix[T int | float64](rowsCount int, colsCount int) (*Matrix[T], error) {
+	if rowsCount == 0 && colsCount == 0 {
+		return nil, errors.New("row and cols count cannot be zero")
+	}
+
+	var arr [][]T
+
+	for i := 0; i < rowsCount; i++ {
+		arr = append(arr, []T{})
+		for j := 0; j < colsCount; j++ {
+			arr[i] = append(arr[i], 0)
+		}
+	}
+
+	return &Matrix[T]{
+		size:  &Size{Cols: colsCount, Rows: rowsCount},
+		array: arr,
+	}, nil
+}
+
+func NewVector[T int | float64](dimension int) *Vector[T] {
+	var v [][]T
+
+	for i := 0; i < dimension; i++ {
+		v = append(v, []T{})
+		v[i] = append(v[i], 0)
+	}
+
+	return &Vector[T]{
+		size:  &Size{Cols: 1, Rows: dimension},
+		array: v,
+	}
+}
+
+func (m *Matrix[T]) Get(row int, col int) (T, error) {
+	if !m.validIndex(row, col) {
+		return 0, errors.New("index is out of array")
+	}
+
+	return m.array[row][col], nil
+}
+
+func (m *Matrix[T]) Set(row int, col int, val T) error {
+	if !m.validIndex(row, col) {
+		return errors.New("index is out of array")
+	}
+	m.array[row][col] = val
+
+	return nil
+}
+
+func (m *Matrix[T]) validIndex(row int, col int) bool {
+	return m.validCol(col) && m.validRow(row)
+}
+
+func (m *Matrix[T]) validRow(row int) bool {
+	return 0 <= row && row < m.size.Rows
+}
+
+func (m *Matrix[T]) validCol(col int) bool {
+	return 0 <= col && col < m.size.Cols
+}
+
+func (m *Matrix[T]) Multiplicate(other *Matrix[T]) (*Matrix[T], error) {
+	if m.size.Cols != other.size.Rows {
+		return nil, errors.New("rows and cols count are not equal")
+	}
+
+	res, err := NewMatrix[T](m.size.Rows, other.size.Cols)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < other.size.Cols; j++ {
+			for k := 0; k < m.size.Cols; k++ {
+				res.array[i][j] += m.array[i][k] * other.array[k][j]
+			}
+		}
+	}
+
+	return res, nil
+}
+
+func (m *Matrix[T]) ScalarMultiplication(val T) *Matrix[T] {
+	res := *m
+
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			res.array[i][j] *= val
+		}
+	}
+
+	return &res
+}
+
+func (m *Matrix[T]) Add(other *Matrix[T]) (*Matrix[T], error) {
+	if m.size.Rows != other.size.Rows || m.size.Cols != other.size.Cols {
+		return nil, errors.New("different sizes")
+	}
+
+	res := *m
+
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			res.array[i][j] += other.array[i][j]
+		}
+	}
+
+	return &res, nil
+}
+
+func (m *Matrix[T]) Subtruct(other *Matrix[T]) (*Matrix[T], error) {
+	if m.size.Rows != other.size.Rows || m.size.Cols != other.size.Cols {
+		return nil, errors.New("different sizes")
+	}
+
+	res := *m
+
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			res.array[i][j] -= other.array[i][j]
+		}
+	}
+
+	return &res, nil
+}
+
+func (m *Matrix[T]) Transpose() *Matrix[T] {
+	res, _ := NewMatrix[T](m.size.Cols, m.size.Rows)
+
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			res.array[j][i] = m.array[i][j]
+		}
+	}
+
+	return res
+}
+
+func (m *Matrix[T]) Exec(f func(x T) T, col int) (*Matrix[T], error) {
+	res := *m
+
+	if col == -1 {
+		for i := 0; i < m.size.Rows; i++ {
+			for j := 0; j < m.size.Cols; j++ {
+				res.array[i][j] = f(res.array[i][j])
+			}
+		}
+	} else {
+		if !m.validCol(col) {
+			return nil, errors.New("col is out of array")
+		}
+		for i := 0; i < m.size.Rows; i++ {
+			res.array[i][col] = f(res.array[i][col])
+		}
+	}
+
+	return &res, nil
+}
+
+func (m *Matrix[T]) GetRow(row int) (*Matrix[T], error) {
+	if !m.validRow(row) {
+		return nil, errors.New("row is out of array")
+	}
+
+	res, _ := NewMatrix[T](1, m.size.Cols)
+	for i := 0; i < m.size.Cols; i++ {
+		res.array[0][i] = m.array[row][i]
+	}
+
+	return res, nil
+}
+
+func (m *Matrix[T]) GetCol(col int) (*Matrix[T], error) {
+	if !m.validCol(col) {
+		return nil, errors.New("col is out of array")
+	}
+
+	res, _ := NewMatrix[T](m.size.Rows, 1)
+	for i := 0; i < m.size.Rows; i++ {
+		res.array[i][0] = m.array[i][col]
+	}
+
+	return res, nil
+}
---
+++ matrix_test.go
@@ -0,0 +1,216 @@
+package linealgebra
+
+import (
+	"math"
+	"testing"
+)
+
+func TestNewMatrix(t *testing.T) {
+	m, _ := NewMatrix[float64](2, 2)
+
+	if m.size.Cols != 2 || m.size.Rows != 2 {
+		t.Error("invalid rows or cols")
+	}
+}
+
+func TestMatrixSetGet(t *testing.T) {
+	m, values := getMatrix[float64](2, 2)
+	_, err := m.Get(-1, 100)
+	if err == nil {
+		t.Error("error was not occurred")
+	}
+
+	k := 0
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			if val, _ := m.Get(i, j); val != values[k] {
+				t.Errorf("invalid value. expected %f, found %f\n", values[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func TestMatrixMultiplicate(t *testing.T) {
+	m1, _ := NewMatrix[int](2, 2)
+	m2, _ := NewMatrix[int](3, 3)
+
+	_, err := m1.Multiplicate(m2)
+	if err == nil {
+		t.Error("error was not occurred")
+	}
+
+	m1, _ = getMatrix[int](2, 2)
+
+	m2, _ = getMatrix[int](2, 3)
+
+	m, err := m1.Multiplicate(m2)
+	ans := []int{9, 12, 15, 19, 26, 33}
+	k := 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %d, found %d\n", ans[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func TestMatrixScalarMultiplication(t *testing.T) {
+	m, _ := getMatrix[int](2, 2)
+
+	m.ScalarMultiplication(5)
+
+	ans := []int{5, 10, 15, 20}
+	k := 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %d, found %d\n", ans[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func TestMatrixAdd(t *testing.T) {
+	m1, _ := getMatrix[int](2, 2)
+	m2, _ := getMatrix[int](3, 3)
+
+	if _, err := m1.Add(m2); err == nil {
+		t.Error("error was not occurred")
+	}
+
+	m2, _ = getMatrix[int](2, 2)
+	m, _ := m1.Add(m2)
+	ans := []int{2, 4, 6, 8}
+	k := 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %d, found %d\n", ans[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func TestMatrixTranspose(t *testing.T) {
+	m, _ := getMatrix[int](2, 2)
+
+	m = m.Transpose()
+
+	ans := []int{1, 3, 2, 4}
+	k := 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %d, found %d\n", ans[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func TestMatrixExec(t *testing.T) {
+	m, _ := getMatrix[float64](2, 2)
+	m.Set(0, 1, 4)
+	m.Set(1, 0, 9)
+	m.Set(1, 1, 16)
+
+	m, _ = m.Exec(func(x float64) float64 { return math.Sqrt(x) }, -1)
+
+	ans := []float64{1, 2, 3, 4}
+	k := 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %f, found %f\n", ans[k], val)
+			}
+			k++
+		}
+	}
+
+	m.Set(0, 1, 4)
+	m.Set(1, 1, 16)
+
+	if _, err := m.Exec(func(x float64) float64 { return math.Sqrt(x) }, 100); err == nil {
+		t.Error("error was not occurred")
+	}
+
+	m, _ = m.Exec(func(x float64) float64 { return math.Sqrt(x) }, 1)
+
+	ans = []float64{1, 2, 3, 4}
+	k = 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %f, found %f\n", ans[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func TestMatrixCol(t *testing.T) {
+	m, _ := getMatrix[int](2, 2)
+
+	if _, err := m.GetCol(100); err == nil {
+		t.Error("error was not occurred")
+	}
+
+	m, _ = m.GetCol(0)
+
+	ans := []int{1, 3}
+	k := 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %d, found %d\n", ans[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func TestMatrixRow(t *testing.T) {
+	m, _ := getMatrix[int](2, 2)
+
+	if _, err := m.GetRow(100); err == nil {
+		t.Error("error was not occurred")
+	}
+
+	m, _ = m.GetRow(0)
+
+	ans := []int{1, 2}
+	k := 0
+	for i := 0; i < m.size.Rows; i++ {
+		for j := 0; j < m.size.Cols; j++ {
+			if val, _ := m.Get(i, j); val != ans[k] {
+				t.Errorf("invalid value. expected %d, found %d\n", ans[k], val)
+			}
+			k++
+		}
+	}
+}
+
+func getMatrix[T int | float64](rows int, cols int) (*Matrix[T], []T) {
+	m, _ := NewMatrix[T](rows, cols)
+
+	var values []T
+
+	for i := 0; i < rows*cols; i++ {
+		values = append(values, T(i+1))
+
+	}
+
+	k := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			m.Set(i, j, values[k])
+			k++
+		}
+	}
+	return m, values
+}
